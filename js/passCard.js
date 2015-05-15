c9c3e6d92e03f9a63773419617eb85b66f3067bd
leonlin14 <leonlin14@gmail.com>
2015-05-16 01:13:14 +0800

    update url

--- js/passCard.js
+++ js/passCard.js
@@ -1,19 +1,26 @@
 var app = angular.module('passCard', []);
 
-app.controller('PassControl', function($scope, $http, $interval) {
-    // $scope.loadData = function() {
-        $http.jsonp('http://localhost:3000/members?q={ "email": "example@gmail.com" }&callback=JSON_CALLBACK', {})
+app.run( function($rootScope, $location, $http) {
+   $rootScope.$watch(function() {
+      return $location.path();
+    },
+    function(a){
+      var url = window.location.href;
+      var index = url.indexOf('#');
+      var token = url.substr(index + 1, 10);
+
+        if(index != -1) {
+          $http.jsonp('http://localhost:3000/members?q={ "token": ' + token + ' }&callback=JSON_CALLBACK', {})
             .success(function(resp) {
-                console.log(resp)
-                $scope.name = resp.members[0].name;
-                $scope.cardno = resp.members[0].cardno;
-        });
-    // };
+                $rootScope.name = resp.members[0].name;
+                $rootScope.cardno = resp.members[0].cardno;
+            });
+        }
 
-    // $scope.loadData();
-    // $interval($scope.loadData, 1000 * 10); // 10 sec
+    });
+});
 
-
+app.controller('PassControl', function($scope, $http, $interval) {
     $scope.message = function() {
         if($scope.cardno) {
             return 'Pass';
