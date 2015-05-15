5321e95391573ce277d6c5c216c3445f4dde7ebe
leonlin14 <leonlin14@gmail.com>
2015-05-15 10:58:14 +0800

    implement pass page

--- js/passCard.js
+++ js/passCard.js
@@ -1,13 +1,24 @@
 var app = angular.module('passCard', []);
 
 app.controller('PassControl', function($scope, $http, $interval) {
-    $scope.loadData = function() {
-        $http.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk').success(function(resp) {
-            console.log(resp);
-            $scope.name = resp.name;
+    // $scope.loadData = function() {
+        $http.jsonp('http://localhost:3000/members?q={ "email": "example@gmail.com" }&callback=JSON_CALLBACK', {})
+            .success(function(resp) {
+                console.log(resp)
+                $scope.name = resp.members[0].name;
+                $scope.cardno = resp.members[0].cardno;
         });
-    };
+    // };
 
-    $scope.loadData();
-    $interval($scope.loadData, 1000 * 10); // 10 sec
+    // $scope.loadData();
+    // $interval($scope.loadData, 1000 * 10); // 10 sec
+
+
+    $scope.message = function() {
+        if($scope.cardno) {
+            return 'Pass';
+        }else {
+            return 'False';
+        }
+    };
 });
