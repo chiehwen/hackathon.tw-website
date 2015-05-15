136362683c9652cf76e971feff951853dca707a4
leonlin14 <leonlin14@gmail.com>
2015-05-15 22:44:37 +0800

    add awards page

--- js/awards.js
+++ js/awards.js
@@ -1,7 +1,7 @@
 var app = angular.module('hackathon', []);
 
 app.controller('AwardsController', function($scope, $http) {
-    $http.jsonp('http://localhost:3000/members?callback=JSON_CALLBACK', {}).success(function(resp) {
+    $http.jsonp('http://localhost:3000/ranking?callback=JSON_CALLBACK', {}).success(function(resp) {
         console.log(resp)
         $scope.members = resp.members;
     });
